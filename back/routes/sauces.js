984597ac76b09e16180ad13bdfb01c1e51036ea2
Quentin Michel <contact@qm-design.fr>
2023-03-28 19:07:58 +0200

    package

--- back/routes/sauces.js
+++ back/routes/sauces.js
@@ -2,19 +2,13 @@ const express = require('express');
 const router = express.Router();
 const saucesCtrl = require('../controllers/sauces');
 const multer = require('../middleware/multer-config');
-
 const auth = require('../middleware/auth');
 
 router.post('/', auth, multer, saucesCtrl.createSauce);
-
 router.put('/:id', auth, multer, saucesCtrl.modifySauce);
-
 router.post('/:id/like', auth, saucesCtrl.likeSauce);
-
 router.delete('/:id', auth, multer, saucesCtrl.deleteSauce);
-
 router.get('/:id', auth, saucesCtrl.getOneSauce);
-
 router.get('/', auth, saucesCtrl.getAllSauce);
 
 module.exports = router;
